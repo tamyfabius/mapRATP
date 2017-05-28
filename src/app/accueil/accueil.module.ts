204531372601faa11e994e118562879f0e1aad12
tamyfabius <tamyfabius@gmail.com>
2017-05-28 08:57:06 +0200

    Ajout du module RouterMoodule dans accueil.module.ts

--- src/app/accueil/accueil.module.ts
+++ src/app/accueil/accueil.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import {AccueilComponent} from './accueil.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [AccueilComponent],
   exports: [AccueilComponent]
 })
